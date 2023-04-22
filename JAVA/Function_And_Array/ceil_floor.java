9b224c8689fb364315a20c887badf266e3786849
Sneha Pandit <snehapandit526@gmail.com>
2023-04-22 12:43:37 +0530

    subset and subarray

---
+++ JAVA/Function_And_Array/ceil_floor.java
@@ -0,0 +1,37 @@
+package JAVA.Function_And_Array;
+import java.util.Scanner;
+public class ceil_floor {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []a = new int[n];
+        for(int i=0; i<a.length; i++) {
+            a[i] = scn.nextInt();
+
+        }
+        int d = scn.nextInt();
+        int low = 0;
+        int high =a.length - 1;
+        int ceil = 0;
+        int floor = 0;
+        while(low<=high){
+            int mid = (low + high) / 2;
+            if(d<a[mid]){
+                high = mid - 1;
+                ceil = a[mid];
+            }
+            else if(d>a[mid]){
+                low = mid - 1;
+                floor = a[mid];
+            }
+            else{
+                ceil = a[mid];
+                floor = a[mid];
+                break;
+            }
+        }
+        System.out.println(ceil);
+        System.out.println(floor);
+        scn.close();
+    }
+}
---
+++ JAVA/Function_And_Array/subset_of_array.java
@@ -0,0 +1,29 @@
+package JAVA.Function_And_Array;
+import java.util.Scanner;
+public class subset_of_array {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []a = new int[n];
+        for(int i=0; i<a.length; i++){
+            a[i] = scn.nextInt();
+
+        }
+        int sub = (int)Math.pow(2,a.length);
+        for(int i=0; i<sub; i++){
+            String set  =" ";
+            int temp = i;
+            for(int j=a.length-1; j>=0; j--){
+                int r = temp%2;
+                temp /= 2;
+                if(r==0){
+                    set = "\t" + set;
+                }else{
+                    set = a[j] + "\t" + set;
+                }
+            }
+            System.out.println(set);
+        }
+        scn.close();
+    }
+}
